d8b8b185dfae7e191400650b3ff736627565d1b7
Anton <antonkzavcenco300@gmail.com>
2023-07-13 07:59:31 +0200

    search results better

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
 import { HistoryService } from "../history/history.service";
 import { PrismaService } from "../prisma.service";
 import { RecommendationService } from "../recommendation/recommendation.service";
@@ -13,26 +14,85 @@ export class SearchService {
   
   async search(query: string) {
     const results = await this.prisma.$transaction([
+      this.prisma.$queryRaw(Prisma.sql`
+     SELECT
+        "Song"."id",
+        "Song"."created_at" AS "createdAt",
+        "Song"."updated_at" AS "updatedAt",
+        "Song"."title",
+        "Song"."cover_big" AS "coverBig",
+        "Song"."cover_medium" AS "coverMedium",
+        "Song"."cover_small" AS "coverSmall",
+        "Song"."duration",
+        "Song"."release_date" AS "releaseDate",
+        "Song"."mp3_path" AS "mp3Path",
+        json_build_object(
+          'id', "Artist"."id",
+          'createdAt', "Artist"."created_at",
+          'updatedAt', "Artist"."updated_at",
+          'name', "Artist"."name",
+          'followers', "Artist"."followers",
+          'pictureBig', "Artist"."picture_big",
+          'pictureMedium', "Artist"."picture_medium",
+          'pictureSmall', "Artist"."picture_small"
+        ) AS "artist"
+      FROM
+        "Song"
+      JOIN
+        "Artist" ON "Song"."artistId" = "Artist"."id"
+      WHERE
+        "Song".title LIKE ${query} OR "Song".title LIKE '%' || ${query} || '%';
+      `),
       this.prisma.$queryRaw`
-      SELECT *
-      FROM "Song"
-    FULL INSERT "Artist" ON "Song"."artistId" = "Artist"."id"
-      WHERE "Song".title LIKE ${query} OR "Song".title LIKE '%' || ${query} || '%';
-      `,
-      this.prisma.$queryRaw`
-      SELECT *
+      SELECT
+      "Album"."id",
+      "Album"."created_at" AS "createdAt",
+      "Album"."updated_at" AS "updatedAt",
+      "Album"."title",
+      "Album"."cover_big" AS "coverBig",
+      "Album"."cover_medium" AS "coverMedium",
+      "Album"."cover_small" AS "coverSmall",
+      "Album"."release_date" AS "releaseDate",
+      "Album"."fans",
+      json_build_object(
+        'id', "Artist"."id",
+        'createdAt', "Artist"."created_at",
+        'updatedAt', "Artist"."updated_at",
+        'name', "Artist"."name",
+        'followers', "Artist"."followers",
+        'pictureBig', "Artist"."picture_big",
+        'pictureMedium', "Artist"."picture_medium",
+        'pictureSmall', "Artist"."picture_small"
+      ) AS "artist"
       FROM "Album"
       LEFT JOIN "Artist" ON "Album"."artistId" = "Artist"."id"
       WHERE "Album".title LIKE ${query} OR "Album".title LIKE '%' || ${query} || '%';
       `,
       this.prisma.$queryRaw`
-      SELECT *
+      SELECT
+      "Playlist"."id",
+      "Playlist"."created_at" AS "createdAt",
+      "Playlist"."updated_at" AS "updatedAt",
+      "Playlist"."title",
+      "Playlist"."cover_big" AS "coverBig",
+      "Playlist"."cover_medium" AS "coverMedium",
+      "Playlist"."cover_small" AS "coverSmall",
+      "Playlist"."fans",
+      "Playlist"."release_date" AS "releaseDate"
       FROM "Playlist"
       WHERE "Playlist".title LIKE ${query} OR "Playlist".title LIKE '%' || ${query} || '%';
       `
       ,
       this.prisma.$queryRaw`
-      SELECT *
+      SELECT
+      "Artist"."id",
+      "Artist"."created_at" AS "createdAt",
+      "Artist"."updated_at" AS "updatedAt",
+      "Artist"."name",
+      "Artist"."followers",
+      "Artist"."picture_big" AS "pictureBig",
+      "Artist"."picture_medium" AS "pictureMedium",
+      "Artist"."picture_small" AS "pictureSmall"
       FROM "Artist"
       WHERE "Artist".name LIKE ${query} OR "Artist".name LIKE '%' || ${query} || '%';
       `
--- src/utils/return-song.object.ts
+++ src/utils/return-song.object.ts
@@ -14,3 +14,5 @@ export const returnSongObject = {
     select: returnArtistObject
   }
 };
+
+
